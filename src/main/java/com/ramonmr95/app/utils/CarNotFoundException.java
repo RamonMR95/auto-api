54c4e986e8105e6384f58c8c949b78ae56f843a6
Ramon Moñino <ramonmr16@gmail.com>
2020-03-24 09:26:52 +0100

    Added custom exception.

---
+++ src/main/java/com/ramonmr95/app/utils/CarNotFoundException.java
@@ -0,0 +1,16 @@
+package com.ramonmr95.app.utils;
+
+public class CarNotFoundException extends Exception {
+	
+	public CarNotFoundException() {
+		
+	}
+
+	public CarNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+	private static final long serialVersionUID = 1L;
+
+}
