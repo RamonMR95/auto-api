9b6d98d287751f9779a164fce83da31209fa3b4b
Ramon Moñino <ramonmr16@gmail.com>
2020-03-24 09:21:40 +0100

    Added CarService CRUD methods.

--- src/main/java/com/ramonmr95/app/services/CarService.java
+++ src/main/java/com/ramonmr95/app/services/CarService.java
@@ -1,20 +1,47 @@
 package com.ramonmr95.app.services;
 
+import java.util.List;
+import java.util.UUID;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.ramonmr95.app.entities.Car;
-
+import com.ramonmr95.app.utils.CarNotFoundException;
 
 @Stateless
 public class CarService {
 
 	@PersistenceContext(unitName = "carPU")
 	private EntityManager em;
-	
+
+	public List<Car> getCars() {
+		return this.em.createQuery("FROM Car", Car.class).getResultList();
+	}
+
+	public Car getCar(UUID id) throws CarNotFoundException {
+		Car car = this.em.find(Car.class, id);
+		if (car != null) {
+			return car;
+		}
+		throw new CarNotFoundException();
+	}
+
 	public void createCar(Car car) {
 		this.em.persist(car);
+	}
+
+	public void updateCar(Car car) throws CarNotFoundException {
+		if (getCar(car.getId()) != null) {
+			this.em.merge(car);
+		}
+	}
+
+	public void deleteCar(UUID id) throws CarNotFoundException {
+		Car car = this.getCar(id);
+		if (car != null) {
+			this.em.remove(car);
+		}
 	}
 }
