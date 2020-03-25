10d256582b9ec2ec6edc0d670b2a6845a135cafb
Ramon Moñino <ramonmr16@gmail.com>
2020-03-25 18:11:55 +0100

    Moved Swagger OpenAPIDefinition to JAXRSConfiguration.

--- src/main/java/com/ramonmr95/app/JAXRSConfiguration.java
+++ src/main/java/com/ramonmr95/app/JAXRSConfiguration.java
@@ -3,7 +3,20 @@ package com.ramonmr95.app;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition (info = 
+@Info(
+      title = "Cars",
+      version = "0.1",
+      description = "Cars API",
+      license = @License(name = "Apache 2.0"),
+      contact = @Contact(name = "Ramón Moñino Rubio", email = "ramonmr16@gmail.com")
+  )
+)
 @ApplicationPath("/")
 public class JAXRSConfiguration extends Application {
 	
