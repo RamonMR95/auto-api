4a9a19a70497a99c06ed31d5147e1223b402de30
Ramon Moñino <ramonmr16@gmail.com>
2020-03-26 13:23:01 +0100

    Added EntityValidationException in case of car invalid values.

---
+++ src/main/java/com/ramonmr95/app/utils/EntityValidationException.java
@@ -0,0 +1,15 @@
+package com.ramonmr95.app.utils;
+
+public class EntityValidationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public EntityValidationException() {
+
+	}
+
+	public EntityValidationException(String message) {
+		super(message);
+	}
+
+}
