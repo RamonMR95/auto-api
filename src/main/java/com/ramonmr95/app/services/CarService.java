b50fa5ea5e3fbf3db3de751efbd1365798411b4c
Ramon Moñino <ramonmr16@gmail.com>
2020-03-31 17:35:29 +0200

    Added CarService validation error messages through EntityValidationException.

--- src/main/java/com/ramonmr95/app/services/CarService.java
+++ src/main/java/com/ramonmr95/app/services/CarService.java
@@ -18,6 +18,8 @@ import javax.validation.ValidatorFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ramonmr95.app.entities.Car;
 import com.ramonmr95.app.utils.EntityNotFoundException;
 import com.ramonmr95.app.utils.EntityValidationException;
@@ -26,12 +28,12 @@ import com.ramonmr95.app.utils.LoggingInterceptor;
 @Stateless
 @Interceptors(LoggingInterceptor.class)
 public class CarService {
-	
+
 	private final Logger log = LogManager.getLogger(CarService.class);
-	
+
 	@EJB
 	private PersistenceService<Car, UUID> persistenceService;
-	
+
 	public List<Car> getCars() {
 		return this.persistenceService.getEntitiesWithNamedQuery("Car.findAll", Car.class);
 	}
@@ -42,16 +44,15 @@ public class CarService {
 			return car;
 		}
 		log.error("Cannot find a car with id: " + id);
-		throw new EntityNotFoundException();
+		throw new EntityNotFoundException("Cannot find a car with id: " + id);
 	}
 
 	public void createCar(Car car) throws EntityValidationException {
 		if (isCarValid(car)) {
 			this.persistenceService.persistEntity(car);
-		}
-		else {
+		} else {
 			log.error("The car does not fulfill all of the validations");
-			throw new EntityValidationException();
+			throw new EntityValidationException(getCarValidationErrorsString(car));
 		}
 	}
 
@@ -59,10 +60,9 @@ public class CarService {
 		getCar(car.getId());
 		if (isCarValid(car)) {
 			this.persistenceService.mergeEntity(car);
-		}
-		else {
+		} else {
 			log.error("The car does not fulfill all of the validations");
-			throw new EntityValidationException();
+			throw new EntityValidationException(getCarValidationErrorsString(car));
 		}
 	}
 
@@ -70,24 +70,35 @@ public class CarService {
 		Car car = this.getCar(id);
 		this.persistenceService.deleteEntity(car);
 	}
-	
+
 	public Map<String, Set<String>> getCarValidationErrors(Car car) {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		Validator validator = factory.getValidator();
 		Set<ConstraintViolation<Car>> validationErrors = validator.validate(car);
 		Set<String> errorsSet = new HashSet<String>();
-		
+
 		for (ConstraintViolation<Car> constraintViolation : validationErrors) {
 			errorsSet.add(constraintViolation.getMessage());
 		}
-		
+
 		Map<String, Set<String>> errors = new HashMap<String, Set<String>>();
 		errors.put("errors", errorsSet);
 		return errors;
 	}
-	
+
+	private String getCarValidationErrorsString(Car car) {
+		ObjectMapper mapper = new ObjectMapper();
+		String errs = null;
+		try {
+			errs = mapper.writeValueAsString(getCarValidationErrors(car));
+		} catch (JsonProcessingException e) {
+			errs = "Validation errors";
+		}
+		return errs;
+	}
+
 	private boolean isCarValid(Car car) {
 		return this.getCarValidationErrors(car).get("errors").isEmpty();
 	}
-	
+
 }
