20301a685e71a362a736f6b66f95bfd5f2197fcd
Ramon Moñino <ramonmr16@gmail.com>
2020-05-08 11:16:44 +0200

    Added CorsFilter.

---
+++ src/main/java/com/ramonmr95/app/filters/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.ramonmr95.app.filters;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+
+@Provider
+@Priority(Priorities.HEADER_DECORATOR)
+public class CorsFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+		response.getHeaders().add("Access-Control-Allow-Origin", "*");
+		response.getHeaders().add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept");
+		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	}
+}
