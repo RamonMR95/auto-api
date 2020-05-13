b54d3630fecca181644597a9c46208d4e746ea0a
Ramon Moñino <ramonmr16@gmail.com>
2020-05-13 12:01:52 +0200

    Added CarResourceAsyncTest.

--- src/test/java/com/ramonmr95/app/AppTest.java
+++ src/test/java/com/ramonmr95/app/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.ramonmr95.app.resources.BrandResourceTest;
+import com.ramonmr95.app.resources.CarResourceAsyncTest;
 import com.ramonmr95.app.resources.CarResourceRestTest;
 import com.ramonmr95.app.resources.CountryResourceTest;
 import com.ramonmr95.app.services.BrandServiceTest;
@@ -12,7 +13,7 @@ import com.ramonmr95.app.services.CarServiceTest;
 import com.ramonmr95.app.services.CountryServiceTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CarResourceRestTest.class, CountryResourceTest.class, BrandResourceTest.class, CarServiceTest.class,
+@SuiteClasses({ CarResourceRestTest.class, CarResourceAsyncTest.class, CountryResourceTest.class, BrandResourceTest.class, CarServiceTest.class,
 		CountryServiceTest.class, BrandServiceTest.class })
 public class AppTest {
 
