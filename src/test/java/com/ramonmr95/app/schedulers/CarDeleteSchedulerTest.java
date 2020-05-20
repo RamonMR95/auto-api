bc6bb255482702d15b9efe4a4e38896515fffdbd
Ramon Moñino <ramonmr16@gmail.com>
2020-05-20 13:29:24 +0200

    Changed delete -> markToDelete in CarResourceRestTest & CarDeleteSchedulerTest.

--- src/test/java/com/ramonmr95/app/schedulers/CarDeleteSchedulerTest.java
+++ src/test/java/com/ramonmr95/app/schedulers/CarDeleteSchedulerTest.java
@@ -1,7 +1,5 @@
 package com.ramonmr95.app.schedulers;
 
-import static org.junit.Assert.fail;
-
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -18,8 +16,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import com.ramonmr95.app.exceptions.EntityNotFoundException;
-import com.ramonmr95.app.exceptions.InvalidUUIDFormatException;
 import com.ramonmr95.app.services.CarService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -57,15 +53,9 @@ public class CarDeleteSchedulerTest {
 	// Valid values
 	@Test
 	public void WhenDeletingCars_ShouldBeExecutedOnce() {
-		try {
-			Mockito.when(this.carService.getCarsWithDeleteFlag()).thenReturn(new ArrayList<>());
-			Mockito.doNothing().when(this.carService).deleteCar(Mockito.any(String.class));
-			this.carDeleteScheduler.deleteCars();
-			Mockito.verify(this.carService, Mockito.times(1)).getCarsWithDeleteFlag();
-		} catch (EntityNotFoundException | InvalidUUIDFormatException e) {
-			fail("Should not get here");
-		}
-
+		Mockito.when(this.carService.getCarsWithDeleteFlag()).thenReturn(new ArrayList<>());
+		this.carDeleteScheduler.deleteCars();
+		Mockito.verify(this.carService, Mockito.times(1)).getCarsWithDeleteFlag();
 	}
 
 }
